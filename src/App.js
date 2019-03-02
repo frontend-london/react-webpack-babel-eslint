1af38e3b608c50c82d990f8cf62ae55dbaf58b43
Piotr Kołodziejczyk <contact@frontend.london>
2019-03-02 21:41:08 +0000

    React configured to use with Webpack, Babel and ESLint

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Basic App demo
+      </div>
+    );
+  }
+}
+export default App;
